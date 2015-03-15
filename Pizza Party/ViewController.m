d48963243fd141916725322daea301881d4486bd
Jason Gregory and Nick Bimpasis <ionicmobile+jason.gregory+nick.bimpasis@asynchrony.com>
2015-03-15 13:18:30 -0500

    Finalizing some notes

--- Pizza Party/ViewController.m
+++ Pizza Party/ViewController.m
@@ -21,7 +21,7 @@
     self.mainTable = [[UITableView alloc] init]; //allocates and initilizes an instance of UITableView called mainTable
     self.mainTable.dataSource = self; //gives mainTable a DataSource
     self.mainTable.delegate = self; //gives mainTable a delegate
-    [self.mainTable registerClass:UITableViewCell.class forCellReuseIdentifier:@"UITableViewCell"]; //Still working on what this does...
+//    [self.mainTable registerClass:UITableViewCell.class forCellReuseIdentifier:@"UITableViewCell"]; //Still working on what this does...
     [self.view addSubview:self.mainTable]; // creats a subView of View called mainTable
 
 }
@@ -32,12 +32,12 @@
 }
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
-    return 10;
+    return 10; //Creating a UITableView called tableView with a definition of how many rows per section
 }
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
-//    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"UITableViewCell"];
-    cell.backgroundColor = [UIColor lightGrayColor];
-//    cell.textLabel.text = [NSString stringWithFormat:@"index: %d", (int)indexPath.row];
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"UITableViewCell"]; //assigns re-usable cells to the variable "cell"
+    cell.backgroundColor = [UIColor lightGrayColor]; //changes the cell color
+    cell.textLabel.text = [NSString stringWithFormat:@"index: %d", (int)indexPath.row]; // assigns the text index "number" based on indexPath.row which by default is an NSInteger
     return cell;
 }
 @end
